60e286d1c4bfd31ae8a1e25b529ffc3ae8c4cf10
Matt Rothmeyer <25441636+TenTonsOfHat@users.noreply.github.com>
2019-11-25 12:35:46 -0600

    Fixed Multiple Entry Issue

--- SetLocationMavenlink.js
+++ SetLocationMavenlink.js
@@ -1,6 +1,6 @@
 
-var regions = [];  
-var entryName = ""
+var regions = [], selectRegions = [];  
+var entryName = "Arkansas"
 var timeoutWait = 100;
 var timeSheetPage = "capspire.mavenlink.com/timesheets"
 
@@ -22,7 +22,7 @@ function LoadRegions(){
     console.log(regions.length)
 
     if(regions.length > 0){
-        entryName = prompt("Please enter your default location.", ""); 
+        entryName = entryName.length > 0 ? entryName : prompt("Please enter your default location.", ""); 
         if(entryName != null && entryName.length > 0){
             if(EditOpen()){
                 ClickSave();
@@ -43,45 +43,53 @@ function OpenInputForRegion(){
         var input = region.children('input').first().val(); 
         var pencil = region.children(".edit-box-link").first();
         pencil.click();
-        WaitFor(EditOpen, OpenSelect)
+        WaitFor(EditOpen, SetSelectRegionItems)
     }else {
         console.log("finished")
     }
 } 
 
+function SetSelectRegionItems(){
+    selectRegions = []; 
+    $.each($('.edit-box').first().find(".location-select-region"), function (i,v){selectRegions.push($(v))})
+    WaitFor(null, OpenSelect)
+}
+
+
 function OpenSelect(){
-    var locationSelect = $('.edit-box').first().find(".location-select-region").first();
-    locationSelect.find('.selectize-input').first().click()
-    WaitFor(DropdownContentOpen, ClickEntry)
+
+    if(selectRegions.length > 0){
+        var selectRegion = $(selectRegions.shift());
+        selectRegion.find('.selectize-input').first().click()
+        WaitFor(DropdownContentOpen, function(){ClickEntry(selectRegion)})
+    }else {
+         WaitFor(null, ClickSave);
+    }
+
 } 
 
 
 
-function ClickEntry() {
+function ClickEntry(locationSelect) {
     var foundLocation = true;
-    var locationSelect = $('.edit-box').first().find(".location-select-region");
-    $.each(locationSelect, function(i,v){
-            var currentValue = $(v).find('#edit-box-location-select').first().val(); 
-            if(currentValue.toLowerCase() != entryName.toLowerCase()){
-               var dropdownContent = $(v)
-                .find('.selectize-dropdown-content').first()
-                .find("[data-value]").filter(function(){return $(this).attr('data-value').toLowerCase() == entryName.toLowerCase();})
-                .first();
-                
-                if(dropdownContent.length >0){
-                    dropdownContent.click();
-                }else {
-                    alert("Cound not find location equal to: " + entryName)
-                    foundLocation = false;
-                    return false;
-                }
-                 
-            }
+    var currentValue = $(locationSelect).find('#edit-box-location-select').first().val(); 
+    if(currentValue.toLowerCase() != entryName.toLowerCase()){
+       var dropdownContent = $(locationSelect)
+        .find('.selectize-dropdown-content').first()
+        .find("[data-value]").filter(function(){return $(this).attr('data-value').toLowerCase() == entryName.toLowerCase();})
+        .first();
 
-    })
+        if(dropdownContent.length >0){
+            dropdownContent.click();
+        }else {
+            alert("Cound not find location equal to: " + entryName)
+            foundLocation = false;
+        }
+
+    }
 
     if(foundLocation){
-        WaitFor(null, ClickSave);
+        WaitFor(null, OpenSelect);
     }
     
     
@@ -109,6 +117,7 @@ function EditOpen(){return $('.edit-box').first().length != 0}
 function EditClosed(){return !EditOpen()}
 function DropdownContentOpen(){return $(".edit-box").find('.selectize-dropdown-content').first().length > 0} 
 function TimeSheetPage(){return window.location.href.includes(timeSheetPage)}
+
 
 LoadRegions();
 
